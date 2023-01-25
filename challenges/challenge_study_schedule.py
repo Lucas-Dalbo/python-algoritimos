163b34001736842555746bae4fc669824e448bbc
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-25 15:27:24 -0300

    feat: Função study_shcedule - Recebe como parametros uma lista com tuplas de inteiros, permanence_period, e um valor inteiro, target_time. - Retorna a quantidade de alunos que estão online na hora escolhida, target_time.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list, target_time: int):
+    students_online = 0
+
+    try:
+        for period in permanence_period:
+            login, logoff = period
+            if login <= target_time and logoff >= target_time:
+                students_online += 1
+        return students_online
+    except TypeError:
+        return None
