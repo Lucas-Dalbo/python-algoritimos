315777db516f908aae3e5898a106af17ee21ed7b
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-25 18:43:06 -0300

    feat: função is_anagram - Recebe duas strings, first e second. - Retorna uma tupla de 3 elementos no formato:   (primeira_palavra_ordenada, segunda_palavra_ordenada, True ou False) O ultimo elemento é True se as palavras forem anagramas e False se não forem.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,48 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_anagram(first_string: str, second_string: str):
+    if first_string == '' and second_string == '':
+        return (first_string, second_string, False)
+
+    first_lower = list(first_string.lower())
+    second_lower = list(second_string.lower())
+    merge_sort(first_lower)
+    merge_sort(second_lower)
+
+    first_lower = "".join(first_lower)
+    second_lower = "".join(second_lower)
+
+    result = first_lower == second_lower
+
+    return (first_lower, second_lower, result)
+
+
+# Merge Sort auxiliar
+# Referência: Course
+def merge_sort(word, start=0, end=None):
+    if end is None:
+        end = len(word)
+    if (end - start) > 1:
+        mid = (end + start) // 2
+        merge_sort(word, start, mid)
+        merge_sort(word, mid, end)
+        merge(word, start, mid, end)
+
+
+def merge(word, start, mid, end):
+    left = word[start:mid]
+    right = word[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            word[general_index] = right[right_index]
+            right_index += 1
+        elif right_index >= len(right):
+            word[general_index] = left[left_index]
+            left_index += 1
+        elif left[left_index] < right[right_index]:
+            word[general_index] = left[left_index]
+            left_index += 1
+        else:
+            word[general_index] = right[right_index]
+            right_index += 1
