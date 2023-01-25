2f0ee7f37f7aa3c0ff067471e62026f41ab5fd98
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-25 16:12:42 -0300

    test: Testa a função encrypt_message Testes para os casos de:  - key com tipo inválido  - message com tipo inválido  - key com valor superior ao tamanho de message  - key com valor par  - key com valor impar

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,47 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+
+INVALID_KEY = '1'
+INVALID_MESSAGE = 1453
+
+VALID_KEY_EVEN = 2
+VALID_KEY_ODD = 3
+VALID_MESSAGE = 'ABCDE'
+
+RESULT_NOT_FOUND = 'EDCBA'
+RESULT_EVEN = 'EDC_BA'
+RESULT_ODD = 'CBA_ED'
 
 
 def test_encrypt_message():
-    pass
+    _test_key_not_int()
+    _test_message_not_sting()
+    _test_key_not_found()
+    _test_key_even()
+    _test_key_odd()
+
+
+def _test_key_not_int():
+    with pytest.raises(expected_exception=TypeError, match="tipo inválido para key"):
+        encrypt_message(VALID_MESSAGE, INVALID_KEY)
+
+
+def _test_message_not_sting():
+    with pytest.raises(expected_exception=TypeError, match="tipo inválido para message"):
+        encrypt_message(INVALID_MESSAGE, VALID_KEY_EVEN)
+
+
+def _test_key_not_found():
+    result = encrypt_message(VALID_MESSAGE, 100)
+    assert result == RESULT_NOT_FOUND
+
+
+def _test_key_even():
+    result = encrypt_message(VALID_MESSAGE, VALID_KEY_EVEN)
+    assert result == RESULT_EVEN
+
+
+def _test_key_odd():
+    result = encrypt_message(VALID_MESSAGE, VALID_KEY_ODD)
+    assert result == RESULT_ODD
