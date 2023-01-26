feb7422cf6a55b48264577002fc7818ac427d322
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-26 11:09:27 -0300

    feat: Função is_palindrome_iterative - Recebe uma string e retorna se ela é um palindromo ou não.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,16 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str):
+    word = str(word)
+    lenght = len(word)
+    if lenght < 1:
+        return False
+
+    low_id = 0
+    high_id = lenght - 1
+
+    while high_id >= low_id:
+        if word[low_id] != word[high_id]:
+            return False
+        low_id += 1
+        high_id -= 1
+
+    return True
