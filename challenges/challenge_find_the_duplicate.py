a7247e9c801cd8b18331b72bae8f667ab471049d
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-26 10:44:01 -0300

    feat: Função find_duplicate - Recebe uma lista de números naturais, zero não incluso. - Retorna o número duplicado na lista. - OBS: Somente um número da lista deve estar se repetindo, se não, ela retorna o maior número duplicado.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums: "list[int]"):
+    nums.sort()
+
+    number, count = [False, 1]
+
+    for index in range(1, len(nums)):
+        cur_number = nums[index]
+
+        if type(cur_number) != int or cur_number < 1:
+            return False
+
+        if cur_number == nums[index - 1]:
+            number = cur_number
+            count += 1
+
+    return number
